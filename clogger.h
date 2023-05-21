0aa072b1a3d9273d766423791405ca88026f9b4c
AllHailTheSheep <benfasick@gmail.com>
2023-05-21 17:50:40 -0400

    implement and test initial functions/settings

--- clogger.h
+++ clogger.h
@@ -1,15 +1,16 @@
 #ifndef clogger
+// TODO: add color, and other log functions
 // this is not on most windows systems, so i'm including it here to make sure things still work
 #ifdef WIN32
 #include <io.h>
 #define F_OK 0
 #define access _access
 #endif
-#include "unistd.h"
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdbool.h"
-#include "time.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 // ----------------------------------------------------------------
 // SETTINGS
 // all settings can (safely) be set through  SET_<SETTING_NAME>() function,
@@ -28,13 +29,20 @@ bool DELETE_ON_RERUN = false;
  */
 int LOG_LEVEL = 0;
 
+
+// prototypes
+void LOG_INIT(char*);
+int SET_DELETE_ON_RERUN(bool);
+int TRACE(char*);
+char* formatter(char*);
+
 FILE* fp;
 
 void LOG_INIT(char* path) {
-    if (access(path, F_OK) && DELETE_ON_RERUN) {
+    if ((access(path, F_OK) == 0) && (DELETE_ON_RERUN == 1)) {
         remove(path);
     }
-    fp = fopen(path, "w+");
+    fp = fopen(path, "a");
 }
 
 int SET_DELETE_ON_RERUN(bool b) {
@@ -44,7 +52,8 @@ int SET_DELETE_ON_RERUN(bool b) {
 
 int SET_LOG_LEVEL(int level) {
     if (level < 0 || level > 7) {
-        printf("CLOGGER ERROR: Invalid log level! Levels must be between 0 and 7! Defaulting to log level 0 (DEBUG)...\n");
+        printf("CLOGGER ERROR: Invalid log level! Levels must be between 0 and 7! Defaulting to log level 0 (TRACE)...\n");
+        LOG_LEVEL = 0;
         return -1;
     } else {
         LOG_LEVEL = level;
@@ -53,6 +62,10 @@ int SET_LOG_LEVEL(int level) {
 }
 
 int TRACE(char* msg) {
+    if (LOG_LEVEL <= 0 && LOG_LEVEL >= 6) {
+        char* f_msg = formatter(msg);
+        fprintf(fp, "TRACE: %s", f_msg);
+    }
 
 
 }
@@ -62,12 +75,14 @@ char* formatter(char* msg) {
     // get time
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
-    char * time = malloc(32 * sizeof(char));
-    snprintf(time, 32, "%d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    char* time = (char*) malloc(32 * sizeof(char));
+    snprintf(time, 32, "%d-%02d-%02d %02d:%02d:%02d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     // constuct message
-    size_t size = sizeof(msg) * sizeof(char);
-    char* f_msg = malloc(size);
-    snprintf(f_msg, size, "%s: %s\n", time, msg);
+    size_t size = (sizeof(msg) * 64) * sizeof(char);
+    char* f_msg = (char*) malloc(size);
+    snprintf(f_msg, size, "%s - %s\n", time, msg);
+    free(time);
+    return f_msg;
 }
 
 #endif
--- test.c
+++ test.c
@@ -1,7 +1,7 @@
 #include "clogger.h"
 
 int main() {
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    printf("now: %d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    SET_DELETE_ON_RERUN(true);
+    LOG_INIT("log.log");
+    TRACE("this is a trace message");
 }
